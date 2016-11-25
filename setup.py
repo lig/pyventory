c6d025d59acb884c092b4666e95e0d422a3cc521
Serge Matveenko <s@matveenko.ru>
2016-11-25 14:28:11 +0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Pyventory',
+    use_scm_version=True,
+    description='Ansible Inventory implementation that uses Python syntax',
+    url='https://gitlab.com/lig/pyventory',
+    author='Serge Matveenko',
+    author_email='s@matveenko.ru',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: System :: Systems Administration',
+    ],
+    keywords='devops ansible inventory',
+    packages=find_packages(),
+    python_requires='>=3.0',
+    setup_requires=['setuptools_scm', 'pytest-runner'],
+    install_requires=[],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pyventory=pyventory.cli:main',
+        ],
+    },
+)
